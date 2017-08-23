9f2fa504b628b5cb15a274ce9c76a4e9a96acebf
sam <sam@silver42.karoo.co.uk>
2017-08-23 01:56:19 +0100

    Add a bunch of other potential languages for translation

---
+++ app/geometrize_hi.ts
@@ -0,0 +1,89 @@
+<TS language="hi" version="2.1">
+<context>
+    <name>AboutDialog</name>
+    </context>
+<context>
+    <name>CommandLineEdit</name>
+    </context>
+<context>
+    <name>CreditsDialog</name>
+    </context>
+<context>
+    <name>GlobalPreferencesDialog</name>
+    </context>
+<context>
+    <name>ImageJobWindow</name>
+    </context>
+<context>
+    <name>LanguageSelectWidget</name>
+    </context>
+<context>
+    <name>LaunchWindow</name>
+    </context>
+<context>
+    <name>LicenseDialog</name>
+    </context>
+<context>
+    <name>OpenUrlDialog</name>
+    </context>
+<context>
+    <name>QWidget</name>
+    </context>
+<context>
+    <name>RecentItemWidget</name>
+    </context>
+<context>
+    <name>RunScriptDialog</name>
+    </context>
+<context>
+    <name>Script evaluation error dialog title</name>
+    </context>
+<context>
+    <name>Script evaluation error message</name>
+    </context>
+<context>
+    <name>Script evaluation unknown error</name>
+    </context>
+<context>
+    <name>ScriptConsole</name>
+    </context>
+<context>
+    <name>ScriptEditorWidget</name>
+    </context>
+<context>
+    <name>SvgPreviewDialog</name>
+    </context>
+<context>
+    <name>TemplateButton</name>
+    </context>
+<context>
+    <name>cli</name>
+    </context>
+<context>
+    <name>geometrize::dialog::AboutDialog</name>
+    </context>
+<context>
+    <name>geometrize::dialog::CreditsDialog</name>
+    </context>
+<context>
+    <name>geometrize::dialog::ImageJobWindow</name>
+    </context>
+<context>
+    <name>geometrize::dialog::LaunchWindow</name>
+    </context>
+<context>
+    <name>geometrize::dialog::RecentItemWidget</name>
+    </context>
+<context>
+    <name>geometrize::dialog::RecentJobsList</name>
+    </context>
+<context>
+    <name>geometrize::dialog::ScriptConsole</name>
+    </context>
+<context>
+    <name>geometrize::dialog::TemplateButton</name>
+    </context>
+<context>
+    <name>geometrize::strings::Strings</name>
+    </context>
+</TS>
